9516c45416c6bb71317c1c8d814dda07191bfe7b
Niels Verdickt <niels.verdickt@outlook.com>
2022-08-24 21:17:26 +0200

    style: add task page styling

--- task.php
+++ task.php
@@ -12,16 +12,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Task</title>
 </head>
 <body>
+    <div class="layout">
+    <a class="back" href="index.php">Back</a>
+
     <?php if(!empty($tasks)): ?>
     <?php foreach($tasks as $task): ?>
             <h1><?php echo $task['title']; ?></h1>
-            <p><?php echo $task['hours']; ?></p>
-            <p><?php echo $task['deadline']; ?></p>
-            <input type="submit" value="Delete Task" name="delete" id="delete">
+            <p>Title: <?php echo $task['title']; ?></p>
+            <p>Hours: <?php echo $task['hours']; ?></p>
+            <p>Deadline: <?php echo $task['deadline']; ?></p>
+            <input type="submit" value="Delete Task" name="delete" id="delete" class="btn">
     <?php endforeach; ?>
     <?php endif ?>
+    </div>
 </body>
 </html>
