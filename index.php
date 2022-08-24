2c3674ff7078f1c6e487426c655b8b1a96b5870f
Niels Verdickt <niels.verdickt@outlook.com>
2022-08-24 20:56:02 +0200

    style: add index page styling

--- index.php
+++ index.php
@@ -16,21 +16,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Index</title>
 </head>
 
 <body>
+<div class="layout">
+    <h1>Tasks</h1>
+    <p>Here you can find all of your tasks. Click on a task to see the details.</p>
     <?php if(!empty($tasks)): ?>
     <?php foreach($tasks as $task): ?>
-        <a href="task.php?id=<?php echo $task['task_id'];?>"><?php echo $task['title']; ?></a>
+        <a href="task.php?id=<?php echo $task['task_id'];?>" class="task"><?php echo $task['title']; ?></a>
     <?php endforeach; ?>
     <?php endif; ?>  
     <?php if(empty($tasks)): ?>
         <p>No lists</p>
     <?php endif ?>
-    
-    <form action="logout.php">
-        <input type="submit" value="Log Out" name="logout" id="logout">
-    </form>
+
+    <a class="addtask" href="add_task.php">+</a>
+
+    <footer class="logout">
+    <a href="logout.php" class="logout">Logout</a>
+    </footer>
+    </div>
 </body>
 </html>
