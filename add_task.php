41c060b12156b0162c663483fad4f76174234aed
Niels Verdickt <niels.verdickt@outlook.com>
2022-08-24 21:32:59 +0200

    style: add add_task page styling

--- add_task.php
+++ add_task.php
@@ -23,17 +23,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Add Task</title>
 </head>
 <body>
+    <div class="layout">
+        <h1>Add A Task</h1>
+        <a class="back" href="index.php">Back</a>
     <form action="" method="post">
-        <div class="signup">
-            <input type="text" id="title" name="title">
-            <input type="number" id="hours" name="hours">
-            <input type="date" id="deadline" name="deadline">
-            <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']?>">
+        <div class="input">
+            <label for="title">Title</label>
+            <input class="input-field" type="text" id="title" name="title">
+            <label for="hours">Hours</label>
+            <input class="input-field" type="number" id="hours" name="hours">
+            <label for="deadline">Deadline</label>
+            <input class="input-field" type="date" id="deadline" name="deadline">
+            <input class="input-field" type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']?>">
         </div>
         <button type="submit" class="btn">Add Task</button>
     </form>
+    </div>
 </body>
 </html>
